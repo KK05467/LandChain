bbcf26180a5e12a49f6e53441bc526b7f400f5f5
KEERTAN KUMAR <keertan2005@gmail.com>
2026-07-10 18:07:15 +0530

    Fix indentation in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <AuthProvider>
           <WalletProvider>
             <App />
-          </WalletProvider>
+         
         </AuthProvider>
       </BrowserRouter>
     </ThemeProvider>
